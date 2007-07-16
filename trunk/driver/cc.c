651ed19d90424b0f9f35faf10784d1c2c5bd0a0e
pcwalton <pcwalton@829ad2bd-f034-0410-bf0f-c23b9ff22ae0>
2007-07-16 00:48:50 +0000

    stub for cc

--- trunk/driver/cc.c
+++ trunk/driver/cc.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+#define TODO_PREPROCESS             1
+#define TODO_COMPILE_TO_BYTECODE    2
+#define TODO_TRANSLATE_BYTECODE     4
+#define TODO_ASSEMBLE               8
+#define TODO_LINK                   16
+
+struct arg {
+    char *data;
+    struct arg *next;
+};
+
+void gather_args(unsigned int *todo, struct arg **compiler_args, struct arg **
+    linker_args)
+{
+     
+}
+
 int main()
 {
+    struct arg *compiler_args, *linker_args;
+    unsigned int todo;
+
+    gather_args(&todo, &compiler_args, &linker_args);
+
     return 0;
 }
 
