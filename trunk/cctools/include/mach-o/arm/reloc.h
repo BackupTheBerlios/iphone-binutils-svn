c81ff6176c1c3bb997f3f8e91e94a76386ae5e4c
pcwalton <pcwalton@829ad2bd-f034-0410-bf0f-c23b9ff22ae0>
2007-07-18 20:30:42 +0000

    fix arm relocs with new info

--- trunk/cctools/include/mach-o/arm/reloc.h
+++ trunk/cctools/include/mach-o/arm/reloc.h
@@ -5,8 +5,12 @@ enum reloc_type_arm
 {
     ARM_RELOC_VANILLA,          /* generic relocation */
     ARM_RELOC_PAIR,             /* second entry of a pair */
-    ARM_RELOC_PCREL_DATA_IMM12, /* Load and Store Word/Immediate Offset, r15 */
+    ARM_RELOC_SECTDIFF,
+    ARM_RELOC_LOCAL_SECTDIFF,
+    ARM_RELOC_UNKNOWN0,
     ARM_RELOC_PCREL_IMM24       /* signed branch offset */
+    ARM_RELOC_UNKNOWN1,
+    ARM_RELOC_PCREL_DATA_IMM12, /* Load and Store Word/Immediate Offset, r15 */
 };
 
 #endif
