bbcd8838af41812e9b741514b7ae8a06e7eacc77
pcwalton <pcwalton@829ad2bd-f034-0410-bf0f-c23b9ff22ae0>
2007-07-18 20:31:57 +0000

    whoops fix syntax error

--- trunk/cctools/include/mach-o/arm/reloc.h
+++ trunk/cctools/include/mach-o/arm/reloc.h
@@ -8,9 +8,9 @@ enum reloc_type_arm
     ARM_RELOC_SECTDIFF,
     ARM_RELOC_LOCAL_SECTDIFF,
     ARM_RELOC_UNKNOWN0,
-    ARM_RELOC_PCREL_IMM24       /* signed branch offset */
+    ARM_RELOC_PCREL_IMM24,      /* signed branch offset */
     ARM_RELOC_UNKNOWN1,
-    ARM_RELOC_PCREL_DATA_IMM12, /* Load and Store Word/Immediate Offset, r15 */
+    ARM_RELOC_PCREL_DATA_IMM12  /* Load and Store Word/Immediate Offset, r15 */
 };
 
 #endif
