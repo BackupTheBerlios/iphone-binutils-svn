a83f759d30132f9fdfaea97ff97e6887bd299455
pcwalton <pcwalton@829ad2bd-f034-0410-bf0f-c23b9ff22ae0>
2007-08-31 03:26:29 +0000

    Lucas Newman's LayerKit patches

---
+++ trunk/include/LayerKit/LayerKit.h
@@ -0,0 +1,28 @@
+#import <LayerKit/LKAnimation.h>
+#import <LayerKit/LKAnimationGroup.h>
+#import <LayerKit/LKBasicAnimation.h>
+#import <LayerKit/LKContext.h>
+#import <LayerKit/LKContextImpl.h>
+#import <LayerKit/LKFilter.h>
+#import <LayerKit/LKKeyframeAnimation.h>
+#import <LayerKit/LKLayer.h>
+#import <LayerKit/LKPropertyAnimation.h>
+#import <LayerKit/LKScrollLayer.h>
+#import <LayerKit/LKSlotProxy.h>
+#import <LayerKit/LKSublayerEnumerator.h>
+#import <LayerKit/LKTimingFunction.h>
+#import <LayerKit/LKTransaction.h>
+#import <LayerKit/LKTransform.h>
+#import <LayerKit/LKTransition.h>
+#import <LayerKit/NSValue-LKTransformAdditions.h>
+#ifndef LAYERKIT_H
+#define LAYERKIT_H
+
+#import <LayerKit/LKLayer.h>
+#import <LayerKit/LKLayer-LKLayerPrivate.h>
+
+int LKPurpleServerIsRunning();
+void LKPurpleServerStart();
+
+#endif
+
