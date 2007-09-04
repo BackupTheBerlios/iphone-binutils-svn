0b569584664487ebe0a465adce737a76af14608e
lllucas <lllucas@829ad2bd-f034-0410-bf0f-c23b9ff22ae0>
2007-09-04 19:15:33 +0000

    Clean up UITextLabel.

--- trunk/include/UIKit/UITextLabel.h
+++ trunk/include/UIKit/UITextLabel.h
@@ -5,60 +5,62 @@
  */
 
 #import <UIKit/UIView.h>
+#import <GraphicsServices/GraphicsServices.h>
 
 @class NSString;
 
-@interface UITextLabel : UIView
-{
-    struct CGSize _size;
-    NSString *_text;
-    struct CGColor *_color;
-    struct CGColor *_highlightedColor;
-    struct CGColor *_shadowColor;
-    struct __GSFont *_font;
-    struct CGSize _shadowOffset;
-    struct {
-        unsigned int ellipsisStyle:3;
-        unsigned int highlighted:1;
-        unsigned int centersHorizontally:1;
-        unsigned int wrapsText:1;
-        unsigned int reserved:26;
-    } _textLabelFlags;
-}
+@interface UITextLabel : UIView {}
 
-+ (struct __GSFont *)defaultFont;	// IMP=0x3245d3e4
-- (struct CGColor *)_disabledFontColor;	// IMP=0x3245da20
-- (void)_invalidateTextSize;	// IMP=0x3245d974
-- (BOOL)centersHorizontally;	// IMP=0x3245dc9c
-- (struct CGColor *)color;	// IMP=0x3245dbec
-- (struct CGColor *)currentTextColor;	// IMP=0x3245dcc4
-- (void)dealloc;	// IMP=0x3245d69c
-- (void)drawContentsInRect:(struct CGRect)fp8;	// IMP=0x3245e160
-- (void)drawRect:(struct CGRect)fp8;	// IMP=0x3245dfa0
-- (struct CGSize)ellipsizedTextSize;	// IMP=0x3245d81c
-- (struct __GSFont *)font;	// IMP=0x3245dedc
-- (struct CGColor *)highlightedColor;	// IMP=0x3245dcbc
-- (BOOL)ignoresMouseEvents;	// IMP=0x3245d734
-- (id)initWithFrame:(struct CGRect)fp8;	// IMP=0x3245d3f8
-- (BOOL)isHighlighted;	// IMP=0x3245df3c
-- (void)setCentersHorizontally:(BOOL)fp8;	// IMP=0x3245dc54
-- (void)setColor:(struct CGColor *)fp8;	// IMP=0x3245dadc
-- (void)setEllipsisStyle:(int)fp8;	// IMP=0x3245df4c
-- (void)setEnabled:(BOOL)fp8;	// IMP=0x3245da58
-- (void)setFont:(struct __GSFont *)fp8;	// IMP=0x3245ddc0
-- (void)setFrame:(struct CGRect)fp8;	// IMP=0x3245d544
-- (void)setHighlighted:(BOOL)fp8;	// IMP=0x3245def4
-- (void)setHighlightedColor:(struct CGColor *)fp8;	// IMP=0x3245dbf4
-- (void)setShadowColor:(struct CGColor *)fp8;	// IMP=0x3245dd64
-- (void)setShadowOffset:(struct CGSize)fp8;	// IMP=0x3245e038
-- (void)setText:(id)fp8;	// IMP=0x3245d990
-- (void)setWrapsText:(BOOL)fp8;	// IMP=0x3245e0a0
-- (struct CGColor *)shadowColor;	// IMP=0x3245ddb8
-- (struct CGSize)shadowOffset;	// IMP=0x3245dee4
-- (void)sizeToFit;	// IMP=0x3245e43c
-- (id)text;	// IMP=0x3245dad4
-- (struct CGSize)textSize;	// IMP=0x3245d73c
-- (BOOL)wrapsText;	// IMP=0x3245dcac
++ (GSFontRef)defaultFont;
 
+- (id)initWithFrame:(CGRect)frame;
+
+- (void)drawContentsInRect:(CGRect)rect;
+- (void)drawRect:(CGRect)rect;
+- (void)sizeToFit;
+
+- (CGColorRef)currentTextColor;
+- (CGSize)textSize;
+- (CGSize)ellipsizedTextSize;
+
+- (BOOL)ignoresMouseEvents;
+
+- (void)setEllipsisStyle:(int)fp8;
+- (void)setEnabled:(BOOL)fp8;
+
+- (BOOL)centersHorizontally;
+- (void)setCentersHorizontally:(BOOL)centersHorizontally;
+
+- (CGColorRef)color;
+- (void)setColor:(CGColorRef)color;
+
+- (GSFontRef)font;
+- (void)setFont:(GSFontRef)font;
+
+- (BOOL)isHighlighted;
+- (void)setHighlighted:(BOOL)isHighlighted;
+
+- (CGColorRef)highlightedColor;
+- (void)setHighlightedColor:(CGColorRef)highlightedColor;	
+	
+- (id)text;
+- (void)setText:(id)text;
+
+- (BOOL)wrapsText;	
+- (void)setWrapsText:(BOOL)wrapsText;	
+
+- (CGColorRef)shadowColor;
+- (void)setShadowColor:(CGColorRef)shadowColor;
+
+- (CGSize)shadowOffset;
+- (void)setShadowOffset:(CGSize)shadowOffset;
+
+- (CGColorRef)_disabledFontColor;	
+- (void)_invalidateTextSize;
+
+@end
+
+@interface UITextLabel (SyntheticEvents)
+- (id)_automationID;
 @end
 
